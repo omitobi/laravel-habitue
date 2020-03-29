1e5dd089c4a799efcbc3a4f30197625e7fc91091
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-03-29 21:06:16 +0300

    Move getter method into a dedicated trait

--- src/Integration/Collector.php
+++ src/Integration/Collector.php
@@ -2,11 +2,14 @@
 
 namespace Harbitue\Integration;
 
+use Harbitue\Traits\Getter;
 use Tightenco\Collect\Support\Collection;
 use Harbitue\Contracts\CollectorInterface;
 
 class Collector extends Collection implements CollectorInterface
 {
+    use Getter;
+
     private array $response;
 
     public function __construct(array $response = [])
@@ -24,24 +27,5 @@ class Collector extends Collection implements CollectorInterface
     public function detach()
     {
         return $this->response;
-    }
-
-    // Trait Getter
-
-    public function __call($method, $parameters)
-    {
-        if (method_exists($this, $method)) {
-            return parent::__call($method, $parameters);
-        }
-
-        if (!(strlen($method) > 3 && strtolower(substr($method, 0, 3)) === 'get')) {
-            return parent::__call($method, $parameters);
-        }
-
-        $value = $this->get(strtolower(substr($method, 3)));
-
-        return is_array($value)
-            ? $this->make($value)
-            : $value;
     }
 }
